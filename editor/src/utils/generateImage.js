6977f6caea6af3fc008092bdd3479aec4f50ade0
Merlin Moelter <joebau.0815@googlemail.com>
2020-06-10 11:37:20 +0200

    Revert web worker changes - Does not work with html2canvas

--- editor/src/utils/generateImage.js
+++ editor/src/utils/generateImage.js
@@ -1,24 +1,8 @@
 import html2canvas from "html2canvas"
 
 async function generateImage(container) {
-    if(window.Worker) {
-        // Use webworker
-        const scriptURL = URL.createObjectURL(new Blob([`(
-            ${async function() {
-                const canvas = await html2canvas(container)
-                postMessage(canvas.toDataURL())
-            }.toString()}
-        )()`], { type: "application/javascript" }))
-
-        const worker = new Worker(scriptURL)
-
-        worker.onmessage = (e) => {
-            console.log(e.data)
-        }
-    } else {
-        const canvas = await html2canvas(container)
-        return canvas.toDataURL()
-    }
+    const canvas = await html2canvas(container)
+    return canvas.toDataURL()
 }
 
 export default generateImage
