38c159db1720203145f05d2e7faf76f0224d9b49
Merlin Moelter <joebau.0815@googlemail.com>
2020-08-03 14:48:18 +0200

    Create Build

---
+++ public/editor/precache-manifest.07b1bb47b38083a88719d32fb1e4dfc6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4419a7012ca1fdf4c18ab556f9aa99d6",
+    "url": "/editor/index.html"
+  },
+  {
+    "revision": "54136c32b14700bfa913",
+    "url": "/editor/static/css/main.1873515d.chunk.css"
+  },
+  {
+    "revision": "6b9ee198623bbf411728",
+    "url": "/editor/static/js/2.71967253.chunk.js"
+  },
+  {
+    "revision": "e62a220b7273527dd9238f8f41d2de9c",
+    "url": "/editor/static/js/2.71967253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54136c32b14700bfa913",
+    "url": "/editor/static/js/main.7e738362.chunk.js"
+  },
+  {
+    "revision": "629a3d8925dee7392924",
+    "url": "/editor/static/js/runtime-main.6589e790.js"
+  },
+  {
+    "revision": "f8b3ffc745a2bc9574a2ab3f93a3584b",
+    "url": "/editor/static/media/FBMessenger.f8b3ffc7.png"
+  },
+  {
+    "revision": "9ee5d43ca8bf61c9522da5179a97a046",
+    "url": "/editor/static/media/Facebook.9ee5d43c.png"
+  },
+  {
+    "revision": "598d07e7761f5bca3a3f78e95b6db422",
+    "url": "/editor/static/media/Gmail.598d07e7.png"
+  },
+  {
+    "revision": "717253e4de6621b80555d907c02a7397",
+    "url": "/editor/static/media/Pinterest.717253e4.png"
+  },
+  {
+    "revision": "3826daa7c52ee865494216fdd046b7ec",
+    "url": "/editor/static/media/Reddit.3826daa7.png"
+  },
+  {
+    "revision": "5a725d789a83feb7a9892a3060df1890",
+    "url": "/editor/static/media/Skype.5a725d78.png"
+  },
+  {
+    "revision": "b4b9d64f2aba775c3ab5bae4279f717f",
+    "url": "/editor/static/media/Telegram.b4b9d64f.png"
+  },
+  {
+    "revision": "93e9138a719d0bc0284e4ece4646613d",
+    "url": "/editor/static/media/Tumblr.93e9138a.png"
+  },
+  {
+    "revision": "1f5a8ef56117a8b2a799bdbed5250039",
+    "url": "/editor/static/media/Twitter.1f5a8ef5.png"
+  },
+  {
+    "revision": "0e878a0fa68c61b06e781cee2e6bc71f",
+    "url": "/editor/static/media/WhatsApp.0e878a0f.svg"
+  },
+  {
+    "revision": "081a833dddd6900fb14232941c638fcd",
+    "url": "/editor/static/media/comic-sans.081a833d.ttf"
+  },
+  {
+    "revision": "8fc622c3a2e2d992ec059cca61e3dfc0",
+    "url": "/editor/static/media/impact.8fc622c3.ttf"
+  }
+]);
